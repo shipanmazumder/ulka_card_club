71fd9c6080257e06085e96e1a154a9f02e880414
Shipan Mazumder <shipanmazumder@gmail.com>
2021-02-18 21:14:20 +0600

    error handle

--- app.js
+++ app.js
@@ -39,33 +39,9 @@ app.use((req,res,next)=>{
         status: 404
     });
 })
-SocketSingleton.io.on("move", (data) => {
-  switch (data) {
-    case "left":
-      position.x -= 5;
-      SocketSingleton.io.emit("position", position);
-      break;
-    case "right":
-      position.x += 5;
-      SocketSingleton.io.emit("position", position);
-      break;
-    case "up":
-      position.y -= 5;
-      SocketSingleton.io.emit("position", position);
-      break;
-    case "down":
-      position.y += 5;
-      SocketSingleton.io.emit("position", position);
-      break;
-  }
-});
 //error handling middleware
 app.use((error,req, res, next) => {
     console.log(error);
-    fs.appendFile('error.log', JSON.stringify(error.message), function (err) {
-      if (err) throw err;
-      console.log('Saved!');
-    });
     getError(error,req, res, next)
 });
 
